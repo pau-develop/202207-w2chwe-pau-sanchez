9b21a4b20922481d66b8a6b8d29b27e3dcde30c1
Pau <103753396+pau-develop@users.noreply.github.com>
2022-07-16 01:43:20 +0200

    Added random bacteria spawner method to Grid class

--- public/Grid/Grid.js
+++ public/Grid/Grid.js
@@ -1,7 +1,9 @@
 class Grid {
   rows;
   columns;
+  population;
   gameGrid = [];
+
   constructor(rows, columns) {
     this.rows = rows;
     this.columns = columns;
@@ -10,6 +12,17 @@ class Grid {
       for (let y = 0; y < columns; y++) {
         this.gameGrid[x].push(0);
       }
+    }
+  }
+
+  spawnBacteria(population) {
+    if (population > (this.rows * this.columns) / 2)
+      throw "Population can never exceed half of the cells available on the grid!";
+    for (let i = 0; i < population; i++) {
+      const randomX = Math.floor(Math.random() * this.rows);
+      const randomY = Math.floor(Math.random() * this.columns);
+      if (this.gameGrid[randomX][randomY] === 1) i--;
+      else this.gameGrid[randomX][randomY] = 1;
     }
   }
 }
