de31cb39d21402ba360283727f7770a608dcc8d6
Pau <103753396+pau-develop@users.noreply.github.com>
2022-07-16 20:54:21 +0200

    Created and tested the checkForRemainingBacteria method

--- public/Grid/Grid.js
+++ public/Grid/Grid.js
@@ -33,61 +33,71 @@ class Grid {
     let shouldDie = true;
     for (let x = 0; x < this.rows; x++) {
       for (let y = 0; y < this.columns; y++) {
-        shouldDie = this.checkNeigboringCells(x, y);
+        shouldDie = this.checkNeighboringBacteria(x, y);
         if (shouldDie) this.nextGenGrid[x][y] = 0;
         else this.nextGenGrid[x][y] = 1;
       }
     }
+    this.gameGrid = this.nextGenGrid;
   }
-  checkNeigboringCells(row, column) {
+  checkNeighboringBacteria(row, column) {
     let aliveNeighbor = 0;
     let deadNeighbor = 0;
     //east
     if (column < this.columns - 1) {
       if (this.gameGrid[row][column + 1] === 0) deadNeighbor++;
       else if (this.gameGrid[row][column + 1] === 1) aliveNeighbor++;
-    }
+    } else deadNeighbor++;
     //south-east
     if (row < this.rows - 1 && column < this.columns - 1) {
       if (this.gameGrid[row + 1][column + 1] === 0) deadNeighbor++;
       else if (this.gameGrid[row + 1][column + 1] === 1) aliveNeighbor++;
-    }
+    } else deadNeighbor++;
     //south
     if (row < this.rows - 1) {
       if (this.gameGrid[row + 1][column] === 0) deadNeighbor++;
       else if (this.gameGrid[row + 1][column] === 1) aliveNeighbor++;
-    }
+    } else deadNeighbor++;
     //south-west
     if (row < this.rows - 1 && column - 1 >= 0) {
       if (this.gameGrid[row + 1][column - 1] === 0) deadNeighbor++;
       else if (this.gameGrid[row + 1][column - 1] === 1) aliveNeighbor++;
-    }
+    } else deadNeighbor++;
     //west
     if (column - 1 >= 0) {
       if (this.gameGrid[row][column - 1] === 0) deadNeighbor++;
       else if (this.gameGrid[row][column - 1] === 1) aliveNeighbor++;
-    }
+    } else deadNeighbor++;
     //north-west
     if (column - 1 >= 0 && row - 1 >= 0) {
       if (this.gameGrid[row - 1][column - 1] === 0) deadNeighbor++;
       else if (this.gameGrid[row - 1][column - 1] === 1) aliveNeighbor++;
-    }
+    } else deadNeighbor++;
     //north
     if (row - 1 >= 0) {
       if (this.gameGrid[row - 1][column] === 0) deadNeighbor++;
       else if (this.gameGrid[row - 1][column] === 1) aliveNeighbor++;
-    }
+    } else deadNeighbor++;
     //north-east
     if (row - 1 >= 0 && column < this.columns - 1) {
       if (this.gameGrid[row - 1][column + 1] === 0) deadNeighbor++;
       else if (this.gameGrid[row - 1][column + 1] === 1) aliveNeighbor++;
-    }
+    } else deadNeighbor++;
     if (aliveNeighbor < 2 && deadNeighbor !== 3)
       //count neighbors
       return true;
     else if (aliveNeighbor === 2 || aliveNeighbor === 3) return false;
     else if (aliveNeighbor > 3) return true;
     else if (deadNeighbor === 3) return false;
+  }
+
+  checkForRemainingBacteria() {
+    for (let x = 0; x < this.rows; x++) {
+      for (let y = 0; y < this.columns; y++) {
+        if (this.gameGrid[x][y] === 1) return false;
+      }
+    }
+    return true;
   }
 }
 
