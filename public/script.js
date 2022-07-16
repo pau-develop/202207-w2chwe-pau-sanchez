363fc2c9b0fc61b69324c9f275e9d6b717371c4e
Pau <103753396+pau-develop@users.noreply.github.com>
2022-07-16 21:50:22 +0200

    Added a time interval between gameloop iterations

--- public/script.js
+++ public/script.js
@@ -1,24 +1,29 @@
 import Grid from "./Grid/Grid.js";
 
 let endOfLife = false;
-let generation = 0;
+let generations = 0;
+let timer = 500;
 
 const testGrid = new Grid(10, 20);
 console.table(testGrid.gameGrid);
-testGrid.spawnBacteria(40);
+testGrid.spawnBacteria(15);
 console.table(testGrid.gameGrid);
 
 const endGame = function () {
   console.log("End of the game ಥ_ಥ");
 };
 
-const gameLoop = function () {
-  testGrid.iterateOverGrid();
-  console.table(testGrid.gameGrid);
-  endOfLife = testGrid.checkForRemainingBacteria();
-  generation++;
-  if (generation >= 10 || endOfLife) endGame();
-  else gameLoop();
-};
+async function gameLoop() {
+  return new Promise(() => {
+    setTimeout(function () {
+      testGrid.iterateOverGrid();
+      console.table(testGrid.gameGrid);
+      endOfLife = testGrid.checkForRemainingBacteria();
+      generation++;
+      if (generations >= 50 || endOfLife) endGame();
+      else gameLoop();
+    }, timer);
+  });
+}
 
 gameLoop();
