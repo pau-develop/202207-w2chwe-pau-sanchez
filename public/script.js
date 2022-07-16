e6a615b23eba3a8b94a2297f73b8b1b4040d58e3
Pau <103753396+pau-develop@users.noreply.github.com>
2022-07-16 18:02:18 +0200

    Tested the checkNeighboringCells method

--- public/script.js
+++ public/script.js
@@ -1,11 +1,13 @@
 import Grid from "./Grid/Grid.js";
 
-const testGrid = new Grid(3, 3);
+const testGrid = new Grid(9, 9);
 
 console.table(testGrid.gameGrid);
 
-testGrid.spawnBacteria(2);
+testGrid.spawnBacteria(40);
 
 console.table(testGrid.gameGrid);
 
 testGrid.iterateOverGrid();
+
+console.table(testGrid.nextGenGrid);
