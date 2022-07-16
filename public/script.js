28372dd0c8ae964ed30e39f7e8ac6165cd2dc74d
Pau <103753396+pau-develop@users.noreply.github.com>
2022-07-16 21:25:07 +0200

    Added a simple game loop

--- public/script.js
+++ public/script.js
@@ -1,10 +1,24 @@
 import Grid from "./Grid/Grid.js";
 
 let endOfLife = false;
+let generation = 0;
 
-const testGrid = new Grid(5, 5);
+const testGrid = new Grid(10, 20);
 console.table(testGrid.gameGrid);
-testGrid.spawnBacteria(10);
+testGrid.spawnBacteria(40);
 console.table(testGrid.gameGrid);
 
-endOfLife = testGrid.checkForRemainingBacteria();
+const endGame = function () {
+  console.log("End of the game ಥ_ಥ");
+};
+
+const gameLoop = function () {
+  testGrid.iterateOverGrid();
+  console.table(testGrid.gameGrid);
+  endOfLife = testGrid.checkForRemainingBacteria();
+  generation++;
+  if (generation >= 10 || endOfLife) endGame();
+  else gameLoop();
+};
+
+gameLoop();
